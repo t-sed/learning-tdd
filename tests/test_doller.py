e3d75bf28f5c6f7ac8606435de4a780da651773d
浮山颯 <so@ukiyamasatsunoMacBook-Pro.local>
2020-11-08 11:27:42 +0900

    change test

--- tests/test_doller.py
+++ tests/test_doller.py
@@ -1,10 +1,9 @@
-from main.money import Dollar
+from src.money import Dollar
 
-class MoneyTest:
-    def test_times(self):
-        five_dollar: Dollar = Dollar(5)
-        five_dollar.times(2)
-        self.assertEqual(five_dollar.amount, 10)
+def test_times():
+    five_dollar: Dollar = Dollar(5)
+    five_dollar.times(2)
+    assert five_dollar.amount == 10
 
 if __name__ == '__main__':
-    MoneyTest().test_times()
+    test_times()
